2cb6fb77f0cdc0e23857bc07d97d4c82ed7839c8
Yuri Tainã <yuritaina.232@gmail.com>
2025-02-11 01:04:32 -0300

    final

--- LFA_Trab03/src/main/java/com/mycompany/lfa_trab03/LFA_Trab03.java
+++ LFA_Trab03/src/main/java/com/mycompany/lfa_trab03/LFA_Trab03.java
@@ -37,34 +37,4 @@ public class LFA_Trab03 {
         a.isWordAccepted("aabb");
         a.isWordAccepted("aab");
     }
-    
-    
-    public static void ex04(){
-        /*
-        Automata a;
-        String alphabet[] = {"a", "b", "c"};
-        String states[] = {"q0", "q1", "q2", "q3", "qf"};
-        
-        a = new Automata(alphabet, states);
-        a.setInitialState("q0");
-        a.addFinalState("q1");
-        a.addFinalState("qf");
-        //current_state, tape_read, stack_read, stack_write, next_state
-        a.setTransition("q0", "a", "", "A", "q0");
-        a.setTransition("q0", "b", "A", "", "q1");
-        a.setTransition("q1", "b", "A", "", "q1");
-        a.setTransition("q1", "b", "", "B", "q2");
-        a.setTransition("q2", "b", "", "B", "q2");
-        a.setTransition("q2", "c", "B", "", "q3");
-        a.setTransition("q3", "c", "B", "", "q3");
-        a.setTransition("q3", "", "", "", "qf");
-        
-        a.isWordAccepted("abba");
-        a.isWordAccepted("aabbbc");
-        a.isWordAccepted("aabbbcc");
-        a.isWordAccepted("aabbc");
-        a.isWordAccepted("aaa");
-        a.isWordAccepted("aabb");
-*/
-    }
 }
